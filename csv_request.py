80b9302fb6b3805b000b60c22f39a17b7716cce4
Rich Maiale <richm1123@gmail.com>
2020-04-04 17:54:02 -0400

    saving to dir

--- csv_request.py
+++ csv_request.py
@@ -21,48 +21,59 @@ dag = DAG(
 )
 
 
-def get_data(url: str):
-    f_name = input("Enter file name")
-    # wget.download(url, path)
-    r = requests.get(url)
-    with open (f_name + '.csv', 'wb') as fi:
-        fi.write(r.content)
+# def name_data(name: str):
+#     f_name = input(name)
+#     return f_name
 
+def get_data(url: str, path: str, f_name: str):
+        # print(fi)
+# def save_csv(fi: str, path: str, f_name: str):
+    file_path = os.path.join(path, f_name)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+        r = requests.get(url)
+        with open(f_name + '.csv', 'wb') as fi:
+            fi.write(r.content)
+    file = open(file_path, "w")
+    file.write(fi)
+    file.close()
 # """gets csv or other file from url and downloads to a particular local path"""
-
-def csv_read(path: str):
-    all_csv = []
-    for full_file in os.listdir(path):
-        if full_file.endswith('.csv'):
-            all_csv.append(full_file)
-        return all_csv
-
-
-def dframe():
-
-
-    t1 = PythonOperator(
-    task_id='get data',
-    python_callable=get_data,
-    provide_context=True,
-    dag=dag)
-
-t2 = PythonOperator(
-    task_id='csv_read',
-    python_callable=csv_read,
-    provide_context=True,
-    dag=dag)
-
-
-t3 = PythonOperator(
-    task_id='dframe',
-    python_callable=dframe,
-    provide_context=True,
-    dag=dag)
+#
+# def csv_read(path: str):
+#     all_csv = []
+#     for full_file in os.listdir(path):
+#         if full_file.endswith('.csv'):
+#             all_csv.append(full_file)
+#         return all_csv
+#
+#
+#
+#
+#     t1 = PythonOperator(
+#     task_id='get data',
+#     python_callable=get_data,
+#     provide_context=True,
+#     dag=dag)
+#
+# t2 = PythonOperator(
+#     task_id='csv_read',
+#     python_callable=csv_read,
+#     provide_context=True,
+#     dag=dag)
+#
+# t3 = PythonOperator(
+#     task_id='dframe',
+#     python_callable=dframe,
+#     provide_context=True,
+#     dag=dag)
 
 # t1 >> t2, t3
 
 
+# name_data('test')
 
 
-get_data('http://pfref.com/tiny/Hd4jZ')
+get_data('http://pfref.com/tiny/Hd4jZ', '/Users/rmaiale/dev/airflow_home/test_dat', 'test')
+
+
+# save_csv(get_data(), '/Users/rmaiale/dev/airflow_home/test_dat', 'test')
