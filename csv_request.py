734c11edca483bf0e98474169b3ca1c50a542508
Rich Maiale <richm1123@gmail.com>
2020-04-04 00:40:13 -0400

    wget request for get_data

--- csv_request.py
+++ csv_request.py
@@ -1,5 +1,13 @@
 from datetime import timedelta
 import os
+from urllib.request import urlopen
+
+import requests
+import contextlib
+
+import wget as wget
+from requests import get
+import csv
 import pandas as pd
 from airflow import DAG
 from airflow.example_dags.example_http_operator import default_args
@@ -13,7 +21,13 @@ dag = DAG(
     schedule_interval=timedelta(days=1),
 )
 
-def csv_read(path):
+
+def get_data(url: str, path: str):
+   """gets csv or other file from url and downloads to a particular local path"""
+   wget.download(url, path)
+
+
+def csv_read(path: str):
     all_csv = []
     for full_file in os.listdir(path):
         if full_file.endswith('.csv'):
@@ -21,7 +35,15 @@ def csv_read(path):
         return all_csv
 
 
-t1 = DummyOperator()
+def dframe():
+
+
+t1 = PythonOperator(
+    task_id='get data',
+    python_callable=get_data,
+    provide_context=True,
+    dag=dag
+)
 
 t2 = PythonOperator(
     task_id='csv_read',
@@ -29,4 +51,16 @@ t2 = PythonOperator(
     provide_context=True,
     dag=dag)
 
-t1 >> t2
+
+t3 = PythonOperator(
+    task_id='dframe',
+    python_callable=dframe,
+    provide_context=True,
+    dag=dag)
+
+t1 >> t2, t3
+
+
+
+
+# get_data('http://pfref.com/tiny/Hd4jZ')
