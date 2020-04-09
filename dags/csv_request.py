909cb650a5f73476bf4c1090c8d1fd04c3868033
Rich Maiale <richm1123@gmail.com>
2020-04-09 07:33:56 -0400

    indiv. tasks for each csv, not moving data though

--- dags/csv_request.py
+++ dags/csv_request.py
@@ -16,21 +16,56 @@ from sqlalchemy import create_engine
 dag = DAG(
     'csv_request',
     default_args=default_args,
-    description='A simple tutorial DAG',
+    description='Fantasy Football ADP DAG',
     schedule_interval=timedelta(days=1),
 )
 
-def name_data(name: str):
-    f_name = input(name)
-    return f_name
 
-def mysql(f_name: str):
-    conn = create_engine('mysql+pymysql://root:yourpassword@localhost:3306/football')
-    df = pd.read_csv('/Users/rmaiale/dev/DataEngineering.Labs.AirflowProject/dags/football_data/adp.csv', delimiter=',')
-    df.to_sql(name=f_name, con=conn, schema='airflow_project', if_exists='append')
+# def name_data(name: str):
+#     f_name = input(name)
+#     return f_name
+
+def psql_adp():
+    conn = create_engine('postgresql+psycopg2://Rich2:Coder20!@localhost:5432/football')
+    df = pd.read_csv('/Users/Rich2/dev/airflow_home/dags/football_data/ADP.csv', delimiter=',')
+    df.to_sql(name="adp", con=conn, schema='football', if_exists='append')
 
 
+def psql_rb():
+    conn = create_engine('postgresql+psycopg2://Rich2:Coder20!@localhost:5432/football')
+    df = pd.read_csv('/Users/Rich2/dev/airflow_home/dags/football_data/rb_data.csv', delimiter=',')
+    df.to_sql(name="rb", con=conn, schema='football', if_exists='append')
 
+
+def psql_wr():
+    conn = create_engine('postgresql+psycopg2://Rich2:Coder20!@localhost:5432/football')
+    df = pd.read_csv('/Users/Rich2/dev/airflow_home/dags/football_data/wr_data.csv', delimiter=',')
+    df.to_sql(name="wr", con=conn, schema='football', if_exists='append')
+
+
+def psql_qb():
+    conn = create_engine('postgresql+psycopg2://Rich2:Coder20!@localhost:5432/football')
+    df = pd.read_csv('/Users/Rich2/dev/airflow_home/dags/football_data/QB_data.csv', delimiter=',')
+    df.to_sql(name="qb", con=conn, schema='football', if_exists='append')
+
+
+def psql_def():
+    conn = create_engine('postgresql+psycopg2://Rich2:Coder20!@localhost:5432/football')
+    df = pd.read_csv('/Users/Rich2/dev/airflow_home/dags/football_data/def_data.csv', delimiter=',')
+    df.to_sql(name="def", con=conn, schema='football', if_exists='append')
+
+
+def psql_te():
+    conn = create_engine('postgresql+psycopg2://Rich2:Coder20!@localhost:5432/football')
+    df = pd.read_csv('/Users/Rich2/dev/airflow_home/dags/football_data/TE_data.csv', delimiter=',')
+    df.to_sql(name="te", con=conn, schema='football', if_exists='append')
+
+    # df = pd.read_csv('/Users/Rich2/dev/airflow_home/dags/football_data/adp.csv')
+    # df.columns = [c.lower() for c in df.columns] #postgres doesn't like capitals or spaces
+    #
+    # engine = create_engine('postgresql://Rich2:Coder20!@localhost:5432/football')
+    # # df.to_sql("my_table_name", engine)
+    # df.to_sql("adp", engine)
 
 
 # def get_data(url: str, path: str, f_name: str):
@@ -54,34 +89,69 @@ def mysql(f_name: str):
 #             all_csv.append(full_file)
 #         return all_csv
 #
-#moved location
+# moved location
 #
+#
+# t1 = PythonOperator(
+#     task_id='name_data',
+#     python_callable=name_data,
+#     provide_context=True,
+#     dag=dag)
 #
 t1 = PythonOperator(
-    task_id='name_data',
-    python_callable=name_data,
+    task_id='psql_adp',
+    python_callable=psql_adp,
     provide_context=True,
     dag=dag)
-#
+
 t2 = PythonOperator(
-    task_id='mysql',
-    python_callable=mysql,
+    task_id='psql_rb',
+    python_callable=psql_rb,
     provide_context=True,
     dag=dag)
+
+t3 = PythonOperator(
+    task_id='psql_wr',
+    python_callable=psql_wr,
+    provide_context=True,
+    dag=dag)
+
+t4 = PythonOperator(
+    task_id='psql_qb',
+    python_callable=psql_qb,
+    provide_context=True,
+    dag=dag)
+
+t5 = PythonOperator(
+    task_id='psql_def',
+    python_callable=psql_def,
+    provide_context=True,
+    dag=dag)
+
+t6 = PythonOperator(
+    task_id='psql_te',
+    python_callable=psql_te,
+    provide_context=True,
+    dag=dag)
+
+
+
+
 #
+
 # t3 = PythonOperator(
 #     task_id='dframe',
 #     python_callable=dframe,
 #     provide_context=True,
 #     dag=dag)
 
-t1 >> t2
+t1.set_downstream([t2, t3, t4, t5, t6])
 
 
-name_data('test')
+# name_data('test')
 
 
-get_data('http://pfref.com/pi/share/k98HH', '/Users/rmaiale/dev/airflow_home/test_dat', 'test')
+# get_data('http://pfref.com/pi/share/k98HH', '/Users/rmaiale/dev/airflow_home/test_dat', 'test')
 
 
 # save_csv(get_data(), '/Users/rmaiale/dev/airflow_home/test_dat', 'test')
